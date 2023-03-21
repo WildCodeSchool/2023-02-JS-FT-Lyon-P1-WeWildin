b80c1113bf3ba0edbe36c9c615bcbcca68a1f251
Francois Chavallard <francois.chavallard@gmail.com>
2023-03-21 11:32:19 +0100

    tentative de script pour les filtres

---
+++ filter.js
@@ -0,0 +1,283 @@
+// Objets contenant tous les éléments des wilders à afficher sur leur profil.
+
+const lilian = {
+  pic: "assets/Lilian.png",
+  lastName: "BATAILLE",
+  firstName: "Lilian",
+  age: 23,
+  bio: ["Titulaire bac STMG.", "Employé restauration pendant 3 ans."],
+  softSkills: ["Esprit d’équipe et communication", "Pensée critique", "Patience", "Ouverture d’esprit", "Connaissance de soi"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Métier en rapport avec mes passions.",
+  projects: [],
+  objectives: "2ème formation pour me spécialiser.",
+  gitHub: "https://github.com/B-lilian",
+  linkedIn: "",
+  mail: "",
+};
+
+const marco = {
+  pic: "assets/Marco.png",
+  lastName: "BERGER",
+  firstName: "Marc-Antoine",
+  age: 24,
+  bio: ["2017-2023 : Presque 6 ans passé à travaillé dans la Grande Distribution (dont 4,5 années à Intermarché);", "2019/2021 : Bac STMG et BTS MCO (Management Commercial Opérationnel);", "2021 : Une expérience de création d'une entreprise avec 2 associés (E-commerce)."],
+  softSkills: ["Etre pro-actif", "Esprit d’équipe et communication", "Logique", "Pensée critique", "Patience"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Reconversion : J'ai découvert le dev par hasard et j'ai tout de suite accroché. Je me suis lancé dans le dev web pour avoir un travail que j'aime, dans lequel je suis épanoui et qui me donne plus de libertés.",
+  objectives: "J'aimerais poursuivre ma formation par une alternance à la Wild afin de m'insérer dans le marché du dev avec déjà une expérience en poche et des compétences plus avancés.",
+  projects: [],
+  gitHub: "https://github.com/HazeFury",
+  linkedIn: "",
+  mail: "",
+};
+
+const françois = {
+  pic: "assets/Francois.png",
+  lastName: "CHAVALLARD",
+  firstName: "François",
+  age: 35,
+  bio: ["J'ai une formation aux métiers de l'audiovisuel, et une activité de quelques années dans ce domaine à divers postes tels que technicien du son, monteur et mixeur.", "J'ai intégré la CAF en 2012 et travaillé dix ans au sein de cet organisme sur quatre postes différents, de téléconseiller à expert technique.", "Aujourd'hui, j'anime des événements pour La Fête du Clip, Des Gens Cools et le théâtre Instant T, et je me forme aux métiers du développement dans le cadre d'une reconversion professionnelle."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Esprit d’équipe et communication", "Logique", "Pensée critique"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python"],
+  resonsWhyDev: "J'avais le projet d'une reconversion professionnelle, et j'ai été attiré par ce domaine pour son aspect technique, mais aussi pour les possibilités de création et les diverses carrières qu'il propose.",
+  projects: ["https://github.com/Yukitaori/Mastermind"],
+  objectives: "Dans l'immédiat, je viserais plutôt une alternance pour une formation de Concepteur Développeur d'Application. Mais qui sait de quoi demain sera fait ?",
+  gitHub: "https://github.com/Yukitaori",
+  linkedIn: "www.linkedin.com/in/françois-chavallard-2979b93b",
+  mail: "francois.chavallard@gmail.com",
+};
+
+const lucie = {
+  pic: "assets/Lucie.png",
+  lastName: "CHEVANCHE",
+  firstName: "Lucie",
+  age: 35,
+  bio: ["Technicienne de laboratoire en analyses médicales, diplômée en 2011."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Esprit d’équipe et communication", "Logique", "Patience"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Parce que je suis attirée depuis toujours par l'informatique sans avoir suivi cette voie et parce que cela correspond beaucoup plus à ma vie actuelle.",
+  projects: [],
+  objectives: "Peut-être alternance d'un an pour consolider cette formation.",
+  gitHub: "https://github.com/LucieChev",
+  linkedIn: "",
+  mail: "",
+};
+
+const aurelie = {
+  pic: "assets/Aurelie.png",
+  lastName: "DEMURE",
+  firstName: "Aurélie",
+  age: 44,
+  bio: ["Infirmière pendant 22 ans, j'ai travaillé dans différentes spécialités, j'ai coordonné des projets et managé des équipes pendant 8 ans.", "J'ai effectué plusieurs formations en lien avec mon métier, je suis diplômée en éducation thérapeutique, en santé au travail et en santé publique (L3)."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Esprit d’équipe et communication", "Patience", "Ouverture d’esprit"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python"],
+  resonsWhyDev: "Par curiosité au départ, puis par passion.",
+  projects: [],
+  objectives: "Recherche d'une alternance pour préparer le titre de concepteur développeur d'applications.",
+  gitHub: "https://github.com/lilipix",
+  linkedIn: "https://www.linkedin.com/in/aurelie-demure/",
+  mail: "aurelie.demure.ad@gmail.com",
+};
+
+const anthony = {
+  pic: "assets/Anthony.png",
+  lastName: "DISCOURS",
+  firstName: "Anthony",
+  age: 25,
+  bio: ["BTS MUC de 2016 à 2018", "Londres durant 9 mois en restauration", "Différents jobs en tant que vendeur (Conforama 9 mois, Celio 5 mois..ect)."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre pro-actif", "Esprit d’équipe et communication", "Flexibilité cognitive", "Ouverture d’esprit"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Appétence pour l'informatique et facilité d'apprendre sur le net qui m'a permis de voir que j'appréciais le développement.",
+  projects: [],
+  objectives: "Alternance.",
+  gitHub: "https://github.com/anthonydscrs",
+  linkedIn: "https://www.linkedin.com/in/anthony-discours/",
+  mail: "andiscours@laposte.net",
+};
+
+const vanessa = {
+  pic: "assets/Vanessa.png",
+  lastName: "GIRAUD",
+  firstName: "Vanessa",
+  age: 35,
+  bio: ["Assistante médicale depuis 2009 dans un centre de lutte contre le cancer.", "Management d'une équipe de 9 AM pendant 3 ans.", "Février 2023 : début WCS"],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Esprit d’équipe et communication", "Patience", "Créativité"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "J'ai toujours été attirée par la tech et le dev web pour la résolution des problèmes et la créativité.",
+  projects: [],
+  objectives: "Stage après la formation avec la WCS et à long terme une alternance pour concepteur d'application web.",
+  gitHub: "https://github.com/VanessaGrd",
+  linkedIn: "",
+  mail: "",
+};
+
+const marcelo = {
+  pic: "assets/Marcelo.png",
+  lastName: "HENRIQUE DA SILVA",
+  firstName: "Marcelo",
+  age: 33,
+  bio: ["Je vis en France depuis plusieurs années, et j'ai décidé qu'il était temps de changer de métier.", "Comme j'ai toujours eu un certain intérêt pour le code, j'ai décidé de faire ma reconversion dans cette domaine.", "Au début, j'ai commencé à apprendre par moi-même et à regarder des tutoriels sur Youtube et différentes ressources, puis je me suis rendu compte que j'avais besoin de trouver une formation pour me guider dans la bonne direction."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Etre pro-actif", "Pensée critique", "Patience"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python"],
+  resonsWhyDev: "J'ai toujours eu un certain intérêt pour le code, mais malheureusement je ne pouvais pas suivre cette voie auparavant, et l'année dernière j'ai eu l'occasion de le découvrir et de me passionner pour ce domaine.",
+  projects: ["https://marceloxhenrique.github.io/project-login2/", "https://marceloxhenrique.github.io/modulo9projetoAwax/"],
+  objectives: "Après la formation, je trouverai une alternance, CDD, ou même CDI, qui me permettra de continuer à apprendre plus et d'acquérir plus d'expérience dans cette domaine.",
+  gitHub: "https://github.com/marceloxhenrique",
+  linkedIn: "",
+  mail: "",
+};
+
+const guillaume = {
+  pic: "assets/Guillaume.png",
+  lastName: "LAMOUREC",
+  firstName: "Guillaume",
+  age: 26,
+  bio: ["2014 : J'ai eu mon bac L", "2014 - 2017 : Licence de langues anglais-japonais", "2017 - 2019 : Travail en restauration", "2019 - 2020 : Bachelor Game Art", "2020 - 2023 : Travail agricole", "2023 : Coucou la Wild Code School"],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Patience", "Ouverture d’esprit", "Créativité"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Parce que ça avait l'air sympa et que j'aime bosser avec les ordis et le monde du numérique.",
+  projects: [],
+  objectives: "Stage / alternance avec la Wild dans une formation spécialisante.",
+  gitHub: "https://github.com/Wraethh",
+  linkedIn: "",
+  mail: "",
+};
+
+const jawad = {
+  pic: "assets/Jawad.png",
+  lastName: "MESSAOUDENE",
+  firstName: "Jawad",
+  age: 30,
+  bio: ["Licence microbiologie", "Technicien de désinfection chez Linde"],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre organisé", "Logique", "Patience", "Connaissance de soi"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "J'ai toujours adoré tout ce qui touche à la technologie depuis très longtemps. Comme j'avais envie de voir un autre monde je me suis dit pourquoi ne pas sauter le pas.",
+  projects: [],
+  objectives: "Alternance.",
+  gitHub: "https://github.com/Nineball69",
+  linkedIn: "",
+  mail: "jawadmessaoudene@gmail.com",
+};
+
+const vivian = {
+  pic: "assets/Vivian.png",
+  lastName: "QUERENET",
+  firstName: "Vivian",
+  age: 29,
+  bio: ["BTS Design Graphique.", "Graphiste 2016-2022.", "Une expérience en UX/UI Design sur un jeu-vidéo étudiant en 2019.", "Community Manager 2017 à 2019.", "Voyage en Amérique du Sud 2022.", "Début à la WCS 2023"],
+  softSkills: ["Autonomie et capacité d’adaptation", "Logique", "Pensée critique", "Patience", "Créativité"],
+  hardSkills: ["HTML", "CSS", "Javascript"],
+  resonsWhyDev: "J'aime l'apprentissage des langues et ai toujours eu un attrait pour la tech. J'aimais la créativité de mes anciens métiers mais ce n'était pas assez technique pour moi. J'aime la résolution de problème.",
+  projects: [],
+  objectives: "Pour l'instant j'envisage l'alternance pour monter encore plus en compétence avant de totalement me lancer dans le milieu pro.",
+  gitHub: "https://github.com/LightQv",
+  linkedIn: "",
+  mail: "",
+};
+
+const yann = {
+  pic: "assets/Yann.png",
+  lastName: "RICHARD",
+  firstName: "Yann",
+  age: 33,
+  bio: ["J'ai bossé 6 ans en tant que juriste après un master 2 en droit et des études de langues.", "J'ai également suivi une formation d'un an dans une école de musique"],
+  softSkills: ["Logique", "Pensée critique", "Patience", "Connaissance de soi", "Créativité"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js", "Python"],
+  resonsWhyDev: "Envie de changer de voie, et de m'orienter vers un domaine qui me corresponde plus.",
+  projects: [],
+  objectives: "J'aimerais poursuivre avec une alternance après la formation.",
+  gitHub: "https://github.com/LocoBajo",
+  linkedIn: "",
+  mail: "",
+};
+
+const pierre = {
+  pic: "assets/Pierre.png",
+  lastName: "SAUMONT",
+  firstName: "Pierre",
+  age: 41,
+  bio: ["D'abord musicien et technicien son, j'ai fini par atterrir par hasard dans le domaine de l'assurance.", "Après 10 ans à gérer des sinistres habitation j'ai enfin décidé de me lancer dans un domaine qui m'a toujours attiré et qui me passionne : le web.", "Ayant un attrait pour tout ce qui est technique, j'ai choisi le métier de développeur web."],
+  softSkills: ["Autonomie et capacité d’adaptation", "Etre pro-actif", "Esprit d’équipe et communication", "Créativité"],
+  hardSkills: ["HTML", "CSS", "JavaScript"],
+  resonsWhyDev: "Pour avoir un métier passionnant et technique et qui évolue constamment.",
+  projects: [],
+  objectives: "D'abord un stage puis trouver un emploi.",
+  gitHub: "https://github.com/Ropie1981",
+  linkedIn: "https://www.linkedin.com/in/pierre-saumont-87bba5182/",
+  mail: "pierre.saumont@gmail.com",
+};
+
+const teodor = {
+  pic: "https://fastly.picsum.photos/id/765/200/300.jpg?hmac=yRNlm1EXdqLX1q3pNM20VW3eARvw3XIoph6gf4qydTk",
+  lastName: "ZEKHOV",
+  firstName: "Teodor",
+  age: 32,
+  bio: ["Ecole de musique pendant 12 années"],
+  softSkills: ["Etre organisé"],
+  hardSkills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+  resonsWhyDev: "Reconversion",
+  projects: [],
+  objectives: "Stage.",
+  gitHub: "https://github.com/teodor-zhekov",
+  linkedIn: "",
+  mail: "",
+}
+
+
+// Array contenant tous les objets des wilders, classés par ordre alphabétique.
+const wilders = [lilian, marco, françois, lucie, aurelie, anthony, vanessa, marcelo, guillaume, jawad, vivian, yann, pierre, teodor];
+
+
+
+// Création d'un array pour lister toutes les soft skills contenues dans les objets wilder.
+let softSkillsGlobalArray = [];
+
+for (let wilder of wilders) {
+  for (let i = 0; i < wilder.softSkills.length; i++) {
+    console.log("softSkills[i] = " + wilder.softSkills[i]);
+    if (softSkillsGlobalArray === [] || softSkillsGlobalArray.includes(wilder.softSkills[i]) === false) {
+      softSkillsGlobalArray.push(wilder.softSkills[i]);
+    }
+  }
+}
+
+softSkillsGlobalArray.sort();
+
+console.log("softSkillsGlobalArray =" + softSkillsGlobalArray);
+console.log(softSkillsGlobalArray.length);
+
+let menu = querySelector("menu");
+
+let softSkillsFilterArray = [];
+let filteredWilders = [];
+
+for (let i = 0; i < softSkillsGlobalArray.length; i++) {
+  let newSoftSkill = document.createElement("li");
+  newSoftSkill.classList.add("softSkillFilter");
+  newSoftSkill.setAttribute("id", "softSkill" + i)
+  newSoftSkill.innerText = softSkillsGlobalArray[i];
+  menu.appendChild(newSoftSkill);
+
+  newSoftSkill.addEventListener("click", function () {
+    newSoftSkill.classList.toggle("selected");
+    let softSkillId = newSoftSkill.getAttribute("id");
+    softSkillsFilterArray.push(softSkillId);
+    for (let wilder of wilders) {
+      for (let j = 0; j < wilder.softSkills.length; j++) {
+        if (newSoftSkill.classList.contains("selected")) {
+          if (softSkillsFilterArray === [] && softSkillsGlobalArray[i] === wilder.softSkills[j]) {
+            filteredWilders.push(wilder.firstName);
+          } else if (softSkillsFilterArray !== [] && softSkillsGlobalArray[i] !== wilder.softSkills[j]) {
+            let wilderIndex = filteredWilders.findIndex(wilder.firstName);
+            filteredWilders.splice(wilderIndex, 1);
+          }
+        } else {
+          if (softSkillsGlobalArray[i] !== wilder.softSkills[j]) {
+            filteredWilders.push(wilder.firstName);
+          }
+        }
+      }
+    }
+  });
+}
